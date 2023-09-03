e59f3006fb680b863a4f05d020a8045f2f6318f2
francescoDeSimone <francesco.desimone@canonical.com>
2023-09-03 18:03:26 +0200

    test github action

--- config/bufferline.nix
+++ config/bufferline.nix
@@ -3,5 +3,11 @@
     enable = true;
     closeCommand = "Bdelete! %d";
     diagnostics = true;
+    offsets = [
+      { filetype = "NvimTree"; }
+      { text = "File Explorer"; }
+      { highlight = "Directory"; }
+      { separator = true; }
+    ];
   };
 }
