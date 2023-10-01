12bab310320e6e45dd8b9dacc7e17397167bc7aa
francescoDeSimone <francesco.desimone@canonical.com>
2023-10-01 15:54:17 +0200

    add wtf

--- config/wtf.nix
+++ config/wtf.nix
@@ -1,20 +1,9 @@
-{ pkgs, fetchFromGitHub, ... }:
-let
-  mypkgs = pkgs.vimPlugins.overrideDerivation
-    (oldAttrs: {
-      src = fetchFromGitHub
-        {
-          owner = "francescoDeSimone";
-          repo = "nixpkgs";
-          rev = "7d33f229c4b6b5b8b2596699e9a2cd92316016b3";
-          sha256 = "";
-        };
-    });
-in
+{ pkgs, ... }:
 {
   config = {
-    extraPlugins = with mypkgs.vimPlugins; [
-      codeium-vim
+    extraPlugins = with pkgs.vimPlugins; [
+      wtf-nvim
     ];
   };
 }
+
