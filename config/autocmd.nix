bebc4d39c407b29109e2d95da069d12679b42bfc
francescoDeSimone <francesco.desimone@canonical.com>
2023-12-26 21:22:21 +0100

    hide lsp suggestion in insert mode

--- config/autocmd.nix
+++ config/autocmd.nix
@@ -2,9 +2,20 @@
   config = {
     autoCmd = [
       {
+        event = [ "InsertLeave" ];
+        pattern = [ "*" ];
+        # idk why show() doesn't work
+        command = ":lua vim.diagnostic.enable(0)";
+      }
+      {
+        event = [ "InsertEnter" ];
+        pattern = [ "*" ];
+        command = ":lua vim.diagnostic.hide()";
+      }
+      {
         event = [ "BufWritePre" ];
         pattern = [ "*" ];
-        command = ":%s/\s\+$//e";
+        command = ":%s/s+$//e";
       }
       {
         event = [ "VimResized" ];
