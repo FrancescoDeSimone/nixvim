3d1681692147988dc2406f4b8563f18d7de93fa0
francescoDeSimone <francesco.desimone@canonical.com>
2024-01-04 02:57:20 +0100

    add unfree

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         nixvimLib = nixvim.lib.${system};
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true;};
         nixvim' = nixvim.legacyPackages.${system};
         nvim = nixvim'.makeNixvimWithModule {
           inherit pkgs;
