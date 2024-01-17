4f3fa79c8cc30474ce65bb51f08c1f3071298df0
fdesi <francesco.desimone@canonical.com>
2024-01-17 12:13:02 -0800

    add snippet; add friendly-snippets

--- config/luasnip.nix
+++ config/luasnip.nix
@@ -1,3 +1,8 @@
+{ pkgs, ... }:
 {
-  plugins.luasnip.enable = true;
+  plugins.luasnip = {
+    enable = true;
+    fromVscode = [{ }]; # See https://github.com/L3MON4D3/LuaSnip#add-snippets
+  };
+  extraPlugins = [ pkgs.vimPlugins.friendly-snippets ];
 }
