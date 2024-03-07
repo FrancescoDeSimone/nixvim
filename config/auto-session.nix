f711e2f6b58778a6e48fbaa9479754788da5f681
francescoDeSimone <francesco.desimone@canonical.com>
2024-03-07 02:57:41 +0100

    update auto-session

--- config/auto-session.nix
+++ config/auto-session.nix
@@ -1,6 +1,6 @@
 {
   plugins.auto-session = {
     enable = true;
-    extraOptions = { auto_session_allowed_dirs = [ "~/git" ]; };
+    extraOptions = { auto_session_allowed_dirs = [ "$HOME/git" ]; };
   };
 }
