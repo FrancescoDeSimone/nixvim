1e131be3a107cad243d7ab9569c27d23b0ce9999
fdesi <francesco.desimone@canonical.com>
2024-05-25 18:09:49 +0200

    update gen.nvim

--- repo/gen-nvim.nix
+++ repo/gen-nvim.nix
@@ -2,13 +2,13 @@
 
 vimUtils.buildVimPlugin rec {
   pname = "gen.nvim";
-  version= "";
+  version = "";
 
   src = fetchFromGitHub {
     owner = "David-Kunz";
     repo = "gen.nvim";
-    hash = "sha256-Bt7yJxToUnPv3JqBwWQeywIbVRqzHBqnu3NUaIxFx/M=";
-    rev = "87fbe811155b90eea58622614809705b966009ad";
+    hash = "sha256-0AEB6im8Jz5foYzmL6KEGSAYo48g1bkFpjlCSWT6JeE=";
+    rev = "bd19cf584b5b82123de977b44105e855e61e5f39";
   };
 
   meta = with lib; {
