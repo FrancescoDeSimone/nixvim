83ec821a5f34569a42d73eaa4687eab40f9bb06e
fdesi <francesco.desimone@canonical.com>
2024-06-11 22:49:25 +0200

    modify indent-blankline

--- config/ui/indent-blankline.nix
+++ config/ui/indent-blankline.nix
@@ -1,4 +1,25 @@
 {
-  plugins.indent-blankline.enable = true;
-  plugins.indent-blankline.settings.scope.enabled = true;
+  plugins = {
+    indent-blankline = {
+      enable = true;
+      settings = {
+        indent = {
+          char = "│"; # "│" or "▎"
+        };
+        scope = {
+          enabled = true;
+          show_start = true;
+        };
+        exclude = {
+          buftypes = ["terminal" "nofile"];
+          filetypes = [
+            "help"
+            "Trouble"
+            "trouble"
+            "notify"
+          ];
+        };
+      };
+    };
+  };
 }
