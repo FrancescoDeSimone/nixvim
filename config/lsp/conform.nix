66ee6cdd3dc06eaa7968624239399eb3bbf85b93
fdesi <francesco.desimone@canonical.com>
2024-06-11 22:49:07 +0200

    add formatter

--- config/lsp/conform.nix
+++ config/lsp/conform.nix
@@ -1,20 +1,27 @@
-{
+{pkgs, ...}: {
+  extraPackages = with pkgs; [
+    alejandra
+    black
+    google-java-format
+    prettierd
+    stylua
+  ];
   plugins.conform-nvim = {
     enable = true;
     notifyOnError = true;
     formattersByFt = {
-      html = [ [ "prettierd" "prettier" ] ];
-      css = [ [ "prettierd" "prettier" ] ];
-      javascript = [ [ "prettierd" "prettier" ] ];
-      javascriptreact = [ [ "prettierd" "prettier" ] ];
-      typescript = [ [ "prettierd" "prettier" ] ];
-      typescriptreact = [ [ "prettierd" "prettier" ] ];
-      java = [ "google-java-format" ];
-      python = [ "black" ];
-      lua = [ "stylua" ];
-      nix = [ "alejandra" ];
-      markdown = [ [ "prettierd" "prettier" ] ];
-      rust = [ "rustfmt" ];
+      html = [["prettierd" "prettier"]];
+      css = [["prettierd" "prettier"]];
+      javascript = [["prettierd" "prettier"]];
+      javascriptreact = [["prettierd" "prettier"]];
+      typescript = [["prettierd" "prettier"]];
+      typescriptreact = [["prettierd" "prettier"]];
+      java = ["google-java-format"];
+      python = ["black"];
+      lua = ["stylua"];
+      nix = ["alejandra"];
+      markdown = [["prettierd" "prettier"]];
+      rust = ["rustfmt"];
     };
   };
 
