0484c0ad90cb80681f7e621fdf16e03fbc841bea
fdesi <francesco.desimone@canonical.com>
2024-10-15 03:34:05 +0200

    replace spectre with grug-far

---
+++ config/utils/grug-far.nix
@@ -0,0 +1,22 @@
+{pkgs, ...}: {
+  config = {
+    extraPlugins = with pkgs.vimPlugins; [
+      grug-far-nvim
+    ];
+    extraConfigLua = ''
+      require('grug-far').setup()
+    '';
+    keymaps = [
+      {
+        key = "<leader>sr";
+        action = ''
+          :lua require('grug-far').open({ prefills = { search = vim.fn.expand("<cword>") } })<cr>
+        '';
+        options = {
+          silent = true;
+          desc = "Search and replace all";
+        };
+      }
+    ];
+  };
+}
