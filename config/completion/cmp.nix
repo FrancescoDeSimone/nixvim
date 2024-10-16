60df02eed636036846c18d5555c9052b13a5b84f
fdesi <francesco.desimone@canonical.com>
2024-10-16 03:54:53 +0200

    change mapping cmp

--- config/completion/cmp.nix
+++ config/completion/cmp.nix
@@ -45,10 +45,9 @@
           "<C-e>" = "cmp.mapping.close()";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<Tab>" = "cmp.mapping.confirm({ select = true })";
           "<C-UP>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
           "<C-Down>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
         };
       };
     };
