45a21b7de4c9c823ecb22fb196ba63717854ead9
fdesi <francesco.desimone@canonical.com>
2024-10-16 03:57:34 +0200

    update noice

--- config/ui/noice.nix
+++ config/ui/noice.nix
@@ -1,35 +1,189 @@
 {
   plugins.noice = {
     enable = true;
-    notify = {
+    cmdline = {
       enabled = true;
+      format = {
+        cmdline = {
+          icon = "";
+          lang = "vim";
+          pattern = "^:";
+        };
+        filter = {
+          icon = "$";
+          lang = "bash";
+          pattern = "^:%s*!";
+        };
+        help = {
+          icon = "";
+          pattern = "^:%s*he?l?p?%s+";
+        };
+        input = {};
+        lua = {
+          icon = "";
+          lang = "lua";
+          pattern = "^:%s*lua%s+";
+        };
+        search_down = {
+          icon = " ";
+          kind = "search";
+          lang = "regex";
+          pattern = "^/";
+        };
+        search_up = {
+          icon = " ";
+          kind = "search";
+          lang = "regex";
+          pattern = "?%?";
+        };
+      };
+      view = "cmdline_popup";
     };
-    messages = {
-      enabled = true; # Adds a padding-bottom to neovim statusline when set to false for some reason
-    };
+    health.checker = true;
     lsp = {
-      message = {
+      documentation = {
+        opts = {
+          format = ["{message}"];
+          lang = "markdown";
+          render = "plain";
+          replace = true;
+          win_options = {
+            concealcursor = "n";
+            conceallevel = 3;
+          };
+        };
+        view = "hover";
+      };
+      hover = {
         enabled = true;
       };
-      progress = {
-        enabled = false;
-        view = "mini";
+
+      message = {
+        enabled = true;
+        view = "notify";
       };
+      override = {
+        "cmp.entry.get_documentation" = false;
+        "vim.lsp.util.convert_input_to_markdown_lines" = false;
+        "vim.lsp.util.stylize_markdown" = false;
+      };
+      progress = {
+        enabled = true;
+        format = "lsp_progress";
+        formatDone = "lsp_progress";
+        view = "mini"; # https://github.com/folke/noice.nvim?tab=readme-ov-file#-views
+      };
+      signature = {
+        enabled = true;
+        autoOpen = {
+          enabled = true;
+          luasnip = true;
+          throttle = 50;
+          trigger = true;
+        };
+      };
+    };
+    markdown = {
+      highlights = {
+        "@%S+" = "@parameter";
+        "^%s*(Parameters:)" = "@text.title";
+        "^%s*(Return:)" = "@text.title";
+        "^%s*(See also:)" = "@text.title";
+        "{%S-}" = "@parameter";
+        "|%S-|" = "@text.reference";
+      };
+      hover = {
+        "%[.-%]%((%S-)%)" = {__raw = "require('noice.util').open";};
+        "|(%S-)|" = {__raw = "vim.cmd.help";};
+      };
+    };
+    messages = {
+      enabled = true;
+      view = "notify";
+      viewError = "notify";
+      viewHistory = "messages";
+      viewSearch = "virtualtext";
+      viewWarn = "notify";
+    };
+    notify = {
+      enabled = true;
+      view = "notify";
     };
     popupmenu = {
-      enabled = true;
       backend = "nui";
+      enabled = true;
+      kindIcons = true;
     };
-    format = {
-      filter = {
-        pattern = [":%s*%%s*s:%s*" ":%s*%%s*s!%s*" ":%s*%%s*s/%s*" "%s*s:%s*" ":%s*s!%s*" ":%s*s/%s*"];
-        icon = "";
-        lang = "regex";
+    presets = {
+      bottom_search = true;
+      command_palette = true;
+      inc_rename = false;
+      long_message_to_split = true;
+      lsp_doc_border = false;
+    };
+    redirect = {
+      filter = {event = "msg_show";};
+      view = "split";
+    };
+
+    smartMove = {
+      enabled = true;
+      excludedFiletypes = ["cmp_menu" "cmp_docs" "notify"];
+    };
+    commands = {
+      errors = {
+        filter = {error = true;};
+        filter_opts = {reverse = true;};
+        opts = {
+          enter = true;
+          format = "details";
+        };
+        view = "split";
       };
-      replace = {
-        pattern = [":%s*%%s*s:%w*:%s*" ":%s*%%s*s!%w*!%s*" ":%s*%%s*s/%w*/%s*" "%s*s:%w*:%s*" ":%s*s!%w*!%s*" ":%s*s/%w*/%s*"];
-        icon = "󱞪";
-        lang = "regex";
+      history = {
+        filter = {
+          any = [
+            {event = "notify";}
+            {error = true;}
+            {warning = true;}
+            {
+              event = "msg_show";
+              kind = [""];
+            }
+            {
+              event = "lsp";
+              kind = "message";
+            }
+          ];
+        };
+        opts = {
+          enter = true;
+          format = "details";
+        };
+        view = "split";
+      };
+      last = {
+        filter = {
+          any = [
+            {event = "notify";}
+            {error = true;}
+            {warning = true;}
+            {
+              event = "msg_show";
+              kind = [""];
+            }
+            {
+              event = "lsp";
+              kind = "message";
+            }
+          ];
+        };
+        filter_opts = {count = 1;};
+        opts = {
+          enter = true;
+          format = "details";
+        };
+        view = "popup";
       };
     };
   };
