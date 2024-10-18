43d267310f4f8ab0309f048528d28be816424c0b
fdesi <francesco.desimone@canonical.com>
2024-10-18 22:12:36 +0200

    re-enable clangd-extensions

--- config/languages/clangd-extensions.nix
+++ config/languages/clangd-extensions.nix
@@ -1,5 +1,44 @@
+# {
+#   plugins.clangd-extensions = { enable = true; };
+#   extraConfigLua = ''
+#     local cmp_nvim_lsp = require("cmp_nvim_lsp")
+#     require("lspconfig").clangd.setup {
+#       on_attach = on_attach,
+#       capabilities = cmp_nvim_lsp.default_capabilities(),
+#       cmd = {
+#         "/usr/bin/clangd",
+#           "--offset-encoding=utf-16",
+#       },
+#     }
+#   '';
+#
+#}
 {
-  plugins.clangd-extensions = { enable = true; };
+  plugins = {
+    clangd-extensions = {
+      enable = true;
+      enableOffsetEncodingWorkaround = true;
+      ast = {
+        roleIcons = {
+          type = "";
+          declaration = "";
+          expression = "";
+          specifier = "";
+          statement = "";
+          templateArgument = "";
+        };
+        kindIcons = {
+          compound = "";
+          recovery = "";
+          translationUnit = "";
+          packExpansion = "";
+          templateTypeParm = "";
+          templateTemplateParm = "";
+          templateParamObject = "";
+        };
+      };
+    };
+  };
   extraConfigLua = ''
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
     require("lspconfig").clangd.setup {
@@ -7,9 +46,8 @@
       capabilities = cmp_nvim_lsp.default_capabilities(),
       cmd = {
         "/usr/bin/clangd",
-          "--offset-encoding=utf-16",
+        "--offset-encoding=utf-16",
       },
     }
   '';
-
 }
