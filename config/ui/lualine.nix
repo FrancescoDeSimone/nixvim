2cfac5555fcf4492356483064f9472a75f1744d6
fdesi <francesco.desimone@canonical.com>
2024-10-18 22:13:06 +0200

    config lualine

--- config/ui/lualine.nix
+++ config/ui/lualine.nix
@@ -4,6 +4,21 @@
     settings = {
       globalstatus = true;
       extensions = ["fzf"];
+      ignoreFocus = ["NvimTree"];
+      theme = "auto";
+      sections = {
+        lualine_a = ["mode"];
+        lualine_b = [
+          "branch"
+          ""
+          "diff"
+          "diagnostics"
+        ];
+        lualine_c = ["filename"];
+        lualine_x = ["encoding" "fileformat" "filetype"];
+        lualine_y = ["progress" "location"];
+        lualine_z = [''os.date("%R")''];
+      };
     };
   };
 }
