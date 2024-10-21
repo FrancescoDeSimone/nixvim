053de7c59ca4f9aa3b6db13c4285ec26b3340bab
fdesi <francesco.desimone@canonical.com>
2024-10-21 03:11:35 +0200

    add diagnostic options

---
+++ config/diagnostics.nix
@@ -0,0 +1,12 @@
+{
+  diagnostics = {
+    update_in_insert = true;
+    severity_sort = true;
+    float = {
+      border = "rounded";
+    };
+    jump = {
+      severity.__raw = "vim.diagnostic.severity.WARN";
+    };
+  };
+}
