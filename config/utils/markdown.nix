9091be35b03f83099362e088e6dff9873aace4db
fdesi <francesco.desimone@canonical.com>
2024-10-21 03:14:01 +0200

    change markdown plugin

--- config/utils/markdown.nix
+++ config/utils/markdown.nix
@@ -1,8 +1,37 @@
-{
+{config, ...}: {
   plugins = {
-    markdown-preview.enable = true;
-    render-markdown = {
+    markview = {
       enable = true;
+      settings = {
+        latex = {
+          enable = true;
+        };
+        checkboxes = {
+          enable = true;
+          checked.text = "✔";
+        };
+        html = {
+          enable = true;
+        };
+        headings = {
+          enable = true;
+          heading_1 = {
+            style = "icon";
+            hl = "MarkviewHeading1";
+          };
+          heading_2 = {
+            style = "icon";
+            hl = "MarkviewHeading2";
+          };
+          heading_3 = {
+            style = "icon";
+            hl = "MarkviewHeading3";
+          };
+        };
+      };
+    };
+    render-markdown = {
+      enable = !config.plugins.markview.enable;
       settings = {
         preset = "obsidian";
         bullet = {
