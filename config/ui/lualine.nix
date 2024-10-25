ae3bf50b6f8920081b5b13193b83aa52563614f0
fdesi <francesco.desimone@canonical.com>
2024-10-25 19:50:58 +0200

    improve lualine

--- config/ui/lualine.nix
+++ config/ui/lualine.nix
@@ -2,6 +2,15 @@
   plugins.lualine = {
     enable = true;
     settings = {
+      options = {
+        disabled_filetypes = {
+          statusline = [
+            "aerial"
+            "dap-repl"
+            "NvimTree"
+          ];
+        };
+      };
       globalstatus = true;
       extensions = ["fzf"];
       ignoreFocus = ["NvimTree"];
@@ -14,7 +23,31 @@
           "diff"
           "diagnostics"
         ];
-        lualine_c = ["filename"];
+        lualine_c = [
+          "filename"
+          # Show active language server
+          {
+            __unkeyed.__raw = ''
+              function()
+                  local msg = ""
+                  local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+                  local clients = vim.lsp.get_active_clients()
+                  if next(clients) == nil then
+                      return msg
+                  end
+                  for _, client in ipairs(clients) do
+                      local filetypes = client.config.filetypes
+                      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                          return client.name
+                      end
+                  end
+                  return msg
+              end
+            '';
+            icon = "";
+            color.fg = "#ffffff";
+          }
+        ];
         lualine_x = ["encoding" "fileformat" "filetype"];
         lualine_y = ["progress" "location"];
         lualine_z = [''os.date("%R")''];
