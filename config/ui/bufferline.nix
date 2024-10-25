37acc889bb18ec23bd8750496cfbe1cedb3f4b54
fdesi <francesco.desimone@canonical.com>
2024-10-25 19:51:25 +0200

    add error bufferline

--- config/ui/bufferline.nix
+++ config/ui/bufferline.nix
@@ -1,9 +1,28 @@
 {
+  config,
+  lib,
+  ...
+}: {
   plugins.bufferline = {
     enable = true;
     settings.options = {
       closeCommand = "Bdelete! %d";
-      diagnostics = "coc";
+      diagnostics = "nvim_lsp";
+      diagnostics_indicator =
+        # Lua
+        ''
+          function(count, level, diagnostics_dict, context)
+            local s = ""
+            for e, n in pairs(diagnostics_dict) do
+              local sym = e == "error" and " "
+                or (e == "warning" and " " or "" )
+              if(sym ~= "") then
+                s = s .. " " .. n .. sym
+              end
+            end
+            return s
+          end
+        '';
       offsets = [
         {filetype = "NvimTree";}
         {text = "File Explorer";}
