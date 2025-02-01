4fbfc585bf3e31076c488753d7af07a9cf107a2f
fdesi <francesco.desimone@canonical.com>
2025-02-01 02:51:28 +0100

    update grug

--- config/utils/grug-far.nix
+++ config/utils/grug-far.nix
@@ -1,23 +1,27 @@
-{pkgs, ...}: {
-  config = {
-    extraPlugins = with pkgs.vimPlugins; [
-      grug-far-nvim
-    ];
-    extraConfigLua = ''
-      require('grug-far').setup()
-      vim.g.maplocalleader = ' '
-    '';
-    keymaps = [
-      {
-        key = "<leader>sr";
-        action = ''
-          :lua require('grug-far').open({ prefills = { search = vim.fn.expand("<cword>") } })<cr>
-        '';
-        options = {
-          silent = true;
-          desc = "Search and replace all";
-        };
-      }
-    ];
+{
+  plugins = {
+    grug-far = {
+      enable = true;
+      lazyLoad = {
+        # settings = {
+        #   cmd = "GrugFar";
+        #   keys = [
+        #     "<leader>sr"
+        #   ];
+        # };
+      };
+    };
   };
+  keymaps = [
+    {
+      key = "<leader>sr";
+      action = ''
+        :lua require('grug-far').open({ prefills = { search = vim.fn.expand("<cword>") } })<cr>
+      '';
+      options = {
+        silent = true;
+        desc = "Search and replace all";
+      };
+    }
+  ];
 }
