a4d7ec3b3a96d959894d003a35cda424456567b5
fdesi <francesco.desimone@canonical.com>
2025-02-01 02:52:08 +0100

    update flakes and nvim-ide

--- repo/nvim-ide.nix
+++ repo/nvim-ide.nix
@@ -5,13 +5,13 @@
 }:
 vimUtils.buildVimPlugin rec {
   pname = "nvim-ide";
-  version = "6d695e5960701c178f03898dbd79041ea9ea375f";
+  version = "ca0278a9c3cbd7eaf8f62d1753c97c858bd01548";
 
   src = fetchFromGitHub {
     owner = "ldelossa";
     repo = "nvim-ide";
-    rev = "6d695e5960701c178f03898dbd79041ea9ea375f";
-    hash = "sha256-ldJk8X1Q1bOqYT4iKd2LGnpIERvltMGjyUNpJW5QUHQ=";
+    rev = "ca0278a9c3cbd7eaf8f62d1753c97c858bd01548";
+    hash = "sha256-jU2vmstsdnjcNItuFJLi8Oao7r1Bpmkkin07/0dVYlA=";
   };
 
   meta = with lib; {
